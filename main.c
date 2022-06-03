066a9be3a9b9f00916eec02bdde9cc231510c813
David Lu <greatatoo@gmail.com>
2022-06-03 16:34:55 +0800

    新增scene

--- main.c
+++ main.c
@@ -48,6 +48,7 @@ void shuffleCard();
 void dealCard();
 
 void showWelcome();
+void playCardScene();
 
 void arrayAppend();
 void bubbleSort();
@@ -58,30 +59,37 @@ void testRenderCard(int gap);
 
 int main(){
 
-    SetConsoleOutputCP(437);
-    srand(time(NULL));
+    SetConsoleOutputCP(437);//印出特殊ASCII字元，參考課程 ch04 p26
+    srand(time(NULL));//亂數，參考課程 ch05 p28
     startGame();
 
     return 0;
 }
 
 /**
- * @brief 開始遊戲
+ * 開始遊戲
  */
 void startGame(){
 
-    int indent = 35;
-    char choice = '9';
+    /*
+     * TODO 把所有有遊戲的變數放於此處
+     */
+    int indent = 35; //縮排空格個數
+    char choice = '?'; //所選擇的選單號碼
 
     do{
         switch(choice){
         case '1':
-            testRenderCard(1);
+            playCardScene();
+            break;
+        case '2':
+            testRenderCard(1);//XXX 純測試用，將來移除
             break;
         }
         clearScreen();
         showWelcome();
 
+        printf("%*s%s\n", indent, " ", "2) Test");
         printf("%*s%s\n", indent, " ", "1) Play");
         printf("%*s%s\n", indent, " ", "0) Quit");
         printf("\n%*s%s", indent, " ", "Choice:");
@@ -89,28 +97,22 @@ void startGame(){
 }
 
 /**
- * @brief 顯示歡迎畫面
+ * 顯示歡迎畫面
  *
  * 預先將歡迎詞放入一個2維陣列，即字串陣列
  * 歡迎詞有多行字串，每個陣列元素存放一行字串
  * 若字串內容有倒斜線\，則手動將之跳脫(escape)
- * 參考課程投影片ch09 p24
+ * 參考課程ch09 p24
  */
 void showWelcome(){
     //歡迎詞
     char *welcome[] = {
         "",
-        //        "__________.__                 __        ___.              __     ________  ____ ",
-        //        "\\______   \\  | _____    ____ |  | __   |   |____    ____ |  | __ \\_____  \\/_   |",
-        //        " |    |  _/  | \\__  \\ _/ ___\\|  |/ /   |   \\__  \\ _/ ___\\|  |/ /  /  ____/ |   |",
-        //        " |    |   \\  |__/ __ \\\\  \\___|    </\\__|   |/ __ \\\\  \\___|    <  /       \\ |   |",
-        //        " |______  /____(____  /\\___  >__|_ \\_______(____  /\\___  >__|_ \\ \\_______ \\|___|",
-        //        "        \\/          \\/     \\/     \\/             \\/     \\/     \\/         \\/    ",
         "            ____  __           __       __           __      ___  ___",
         "           / __ )/ /___ ______/ /__    / /___ ______/ /__   |__ \\<  /",
         "          / __  / / __ `/ ___/ //_/_  / / __ `/ ___/ //_/   __/ // / ",
         "         / /_/ / / /_/ / /__/ ,< / /_/ / /_/ / /__/ ,<     / __// /  ",
-        "        /_____/_/\\__,_/\\___/_/|_|\\____/\\__,_/\\___/_/|_|   /____/_/   ",
+        "        /_____/_/\\__,_/\\___/_/|_|\\____/\\__,_/\\___/_/|_|   /____/_/",
         "",
     };
 
@@ -124,8 +126,66 @@ void showWelcome(){
 }
 
 /**
- * @brief 測試卡牌渲染
- * @param gap 兩張卡牌間的間隔
+ * 進入一場牌局的場景
+ */
+void playCardScene(){
+    /*
+     * TODO 將牌局所有要用到的變數在此宣告和初始化
+     */
+    int indent = 35;
+    char choice = '?';
+
+    //TODO 從檔案載入所有Player資料到陣列
+    //TODO 要求使用者輸入帳號
+    //TODO 從Player陣列裏搜尋帳號資料 (此處要用到二元搜尋法)
+    //TODO 如果找到帳號資料則 assign 到變數
+    //TODO 如果沒找到則append一個元素到Player陣列
+
+    clearScreen();
+    printf("%*s%s choice=%c\n\n", indent, " ", "User Info", choice);
+    //TODO 顯示Player資料
+
+    do{
+        switch(choice){
+        case '1':
+            //TODO 初始牌堆
+            //TODO 洗牌
+            //TODO 發牌
+            //TODO 進入遊戲邏輯
+            clearScreen();
+            printf("%*s%s\n\n", indent, " ", "PlayCardScene");
+            printf("%*s%s\n", indent, " ", "1) Play again");
+            printf("%*s%s\n", indent, " ", "0) Quit");
+            printf("\n%*s%s", indent, " ", "Choice:");
+            break;
+        case '?':
+            printf("%*s%s\n", indent, " ", "1) GO");
+            printf("%*s%s\n", indent, " ", "0) Quit");
+            printf("\n%*s%s", indent, " ", "Choice:");
+            break;
+        }
+
+    }while((choice=getchar())!='0');
+}
+
+/**
+ * 清除畫面
+ */
+void clearScreen(){
+    system("cls");
+}
+
+/**
+ * 按下任一鍵繼續
+ */
+void pressAnyKeyToContinue(){
+    system("pause");
+}
+
+//---------------以下測試用，將來會移除--------------------
+/**
+ * 測試卡牌渲染(測試用，將來會移除)
+ * gap = 兩張卡牌間的間隔
  */
 void testRenderCard(int gap){
     clearScreen();
@@ -149,18 +209,3 @@ void testRenderCard(int gap){
 
     pressAnyKeyToContinue();
 }
-
-/**
- * @brief 清除畫面
- */
-void clearScreen(){
-    system("cls");
-}
-
-/**
- * @brief 按下任一鍵繼續
- */
-void pressAnyKeyToContinue(){
-    system("pause");
-}
-
--- test.c
+++ test.c
@@ -1,8 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+#define ARRAY_SIZE 2
 
-    printf("Hello World\n");
+void testDummy2dArray();
+void dump2dArr(int* arr, int row, int col);
+
+int main(){
+    testDummy2dArray();
     return 0;
 }
+
+void testDummy2dArray(){
+    int arr[][ARRAY_SIZE]={{1,2},{3,4},{5,6},{7,8}};
+    int row = sizeof(arr)/sizeof(*arr); // 32/8=4
+    int col = ARRAY_SIZE; // 2
+    dump2dArr(*arr, row, col);
+
+    printf("arr %p\n", arr);
+    printf("*arr %p\n", *arr);
+    printf("arr[0] %p\n", arr[0]);
+    printf("&(*arr)[0] %p\n", &(*arr)[0]);
+    printf("&arr[0][0] %p\n", &arr[0][0]);
+
+    /*----- Output -----
+      array[0][0] = 1
+      array[0][1] = 2
+      array[1][0] = 3
+      array[1][1] = 4
+      array[2][0] = 5
+      array[2][1] = 6
+      array[3][0] = 7
+      array[3][1] = 8
+      arr 000000F6BDCFFC48
+      *arr 000000F6BDCFFC48
+      arr[0] 000000F6BDCFFC48
+      &(*arr)[0] 000000F6BDCFFC48
+      &arr[0][0] 000000F6BDCFFC48
+     */
+}
+
+void dump2dArr(int* arr, int row, int col){
+    int i,j;
+    for(i=0; i<row; i++){
+        for(j=0; j<col; j++){
+            printf("array[%d][%d] = %d\n",i,j, *(arr+i*col+j));
+        }
+    }
+}
+
+
